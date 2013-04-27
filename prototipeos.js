c210afdb18684e5c90fd4c97634f773b23425c06
unknown <Administrador@.(none)>
2013-04-27 16:58:01 +0200

    algoritmo hecho

--- prototipeos.js
+++ prototipeos.js
@@ -13,29 +13,58 @@ var escenario = new Kinetic.Stage({
 
 var capa = new Kinetic.Layer();
 
-$(document).ready(function() {
+var puntosDestino = [{
+	x : 73,
+	y : 160
+}, {
+	x : 300,
+	y : 160
+},
+{
+	x: 350,
+	y: 100
+},
+{
+	x: 400,
+	y: 90
+},
+{
+	x: 800,
+	y: 130
+}];
 
-	var puntosDestino = [{
-		x : 73,
-		y : 160
-	}, {
-		x : 300,
-		y : 160
-	}];
+var vertices = [];
+var contador = 1;
 
-	var blueSpline = new Kinetic.Spline({
-		points : [{
-			x : 73,
-			y : 160
-		}, {
-			x : 80,
-			y : 160
-		}],
-		stroke : 'black',
-		strokeWidth : 10,
-		lineCap : 'round',
-		tension : 1
+function mover(elemento) {
+	elemento.transitionTo({
+		x : puntosDestino[contador].x,
+		y : puntosDestino[contador].y,
+		duration : 1,
+		easing : 'ease-in-out',
+		callback : function() {
+			if (contador + 1 < puntosDestino.length) {
+				var nuevoVertice = new Kinetic.Circle({
+					x : puntosDestino[contador].x,
+					y : puntosDestino[contador].y,
+					radius : 10,
+					fill : 'red',
+					stroke : 'black',
+					strokeWidth : 4,
+					draggable : true
+				});
+				contador++;
+				capa.add(nuevoVertice);
+				capa.draw();
+				vertices.push(nuevoVertice);
+				mover(nuevoVertice);
+			}
+		}
 	});
+}
+
+
+$(document).ready(function() {
 
 	var verticeA = new Kinetic.Circle({
 		x : 73,
@@ -43,20 +72,32 @@ $(document).ready(function() {
 		radius : 10,
 		fill : 'red',
 		stroke : 'black',
-		strokeWidth : 4,
-		draggable: true
+		strokeWidth : 4
 	});
 
 	var verticeB = new Kinetic.Circle({
-		x : 300,
+		x : 73,
 		y : 160,
 		radius : 10,
 		fill : 'red',
 		stroke : 'black',
-		strokeWidth : 4,
-		draggable: true
+		strokeWidth : 4
 	});
 	
+	var blueSpline = new Kinetic.Spline({
+		points : [{
+			x : verticeA.getX(),
+			y : verticeA.getY()
+		}],
+		stroke : 'black',
+		strokeWidth : 10,
+		lineCap : 'round',
+		tension : 0.4
+	});
+
+	vertices.push(verticeA);
+	vertices.push(verticeB);
+
 	capa.add(blueSpline);
 	capa.add(verticeA);
 	capa.add(verticeB);
@@ -64,23 +105,23 @@ $(document).ready(function() {
 	escenario.add(capa);
 
 	var animacion = new Kinetic.Animation(function(frame) {
-		
+
 		var puntos = [];
 		
-		puntos[0] = {
-			x: verticeA.getX(),
-			y: verticeA.getY()
-		};
-		
-		puntos[1] = {
-			x: verticeB.getX(),
-			y: verticeB.getY()
-		};
-		
+		for(var i = 0; i < vertices.length;i++)
+		{
+			puntos.push({
+				x: vertices[i].getX(),
+				y: vertices[i].getY()
+			});
+		}
+
 		blueSpline.setPoints(puntos);
-		
+
 	}, capa);
 
 	animacion.start();
+	
+	mover(verticeB);
 
 });
