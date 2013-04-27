7dcc3b82111e3ec68024f0fa4de08fed873b0494
unknown <Administrador@.(none)>
2013-04-27 11:46:49 +0200

    habemus canvas

---
+++ mainJuego.js
@@ -0,0 +1,15 @@
+
+var escenario = new Kinetic.Stage({
+	container : 'container',
+	width : 900,
+	height : 575,
+	draggable : true,
+	dragBoundFunc : function(pos) {
+		return {
+			x : this.getAbsolutePosition().x,
+			y : this.getAbsolutePosition().y
+		}
+	}
+});
+
+var capa = new Kinetic.Layer();
