3f6852aec2c47b005cc8895c341cacdbccd16000
unknown <Axteroide@.(none)>
2013-04-27 12:23:20 +0200

    Funcion carga de imagenes

--- mainJuego.js
+++ mainJuego.js
@@ -21,3 +21,31 @@ $(document).ready(function () {
 	escenario.add(capa);
 	
 });
+
+function cargarImagenes() {
+	$.ajax({
+		type : "GET",
+		url : "images.xml",
+		dataType : "xml",
+		success : function(xml) {
+			debug("hola");
+			var compruebaCargadas;
+			var numeroImagenes = $(xml).find('imagen').length;
+			$(xml).find('imagen').each(function() {
+				var nombreImagen = $(this).text();
+				
+				var imagen = new Image();
+				imagen.onload = function() {
+					dictImg[nombreImagen] = imagen;
+					
+					compruebaCargadas += ".";
+					if (compruebaCargadas.length == numeroImagenes) {
+						//logicaJuego();
+					}
+				};	
+				imagen.src = "img/" + nombreImagen + ".png";	
+			});
+		}
+	});
+
+}
